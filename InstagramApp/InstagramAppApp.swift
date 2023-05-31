5a6c5049ca0c472318c389a11da381e3ef34045c
Guilherme Gatto <guuilhermegatto@hotmail.com>
2023-05-31 15:29:35 -0300

    Initial Commit

---
+++ InstagramApp/InstagramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramAppApp.swift
+//  InstagramApp
+//
+//  Created by Guilherme Gatto on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
